9b07b7cd13091695dc337b7ae924471cb6f09718
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2023-10-09 21:02:32 +0300

    Add project files.

---
+++ hm12_delegate/PrintPersonInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hm12_delegate
+{
+    public class PrintPersonInfo
+    {
+        public void PrintName(string name)
+        {
+            Console.WriteLine(name);
+        }
+        public void PrintBirthDate(string birthDate)
+        {
+            Console.WriteLine(birthDate);
+        }
+        public void PrintPhone(string phone)
+        {
+            Console.WriteLine(phone);
+        }
+
+    }
+}
---
+++ hm12_delegate/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using hm12_delegate;
+
+PrintPersonInfo person = new PrintPersonInfo();
+
+ReaderDelegate[] rd = new ReaderDelegate[3] { person.PrintName, person.PrintBirthDate, person.PrintPhone };
+
+string[] str = { "Oleg", "12.12.1900", "093123413" };
+
+for (int i = 0; i < 3; ++i)
+{
+    rd[i](str[i]);
+}
